be8df89eb9f10befc489846808ca91f6de012679
Chip Reed <chip@chip.sh>
2022-05-06 11:37:28 -0700

    update fuzzer api
    Signed-off-by: Chip Reed <chip@chip.sh>

--- fuzz/src/main.rs
+++ fuzz/src/main.rs
@@ -17,9 +17,7 @@ fn main() {
         let simple = {
             let mut hasher = ::blake3_simple::Hasher::new();
             hasher.update(data);
-            let mut out = [0; 32];
-            hasher.finalize(&mut out);
-            out
+            *hasher.finalize().as_bytes()
         };
 
         for ((&reference, standard), simple) in reference.iter().zip(standard).zip(simple) {
